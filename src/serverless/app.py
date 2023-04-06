7660af43ae936e324b0250b38c93cecd9e2498d8
jacopotagliabue <tagliabue.jacopo@gmail.com>
2023-04-06 11:20:13 -0400

    first commit from older stuff

---
+++ src/serverless/app.py
@@ -0,0 +1,84 @@
+import uuid
+import os
+import time
+import duckdb
+import pandas as pd
+
+
+con = None # global conn object - we re-use this across calls
+DEFAULT_LIMIT = 20 # if we don't specify a limit, we will return at most 20 results
+
+
+def return_duckdb_connection():
+    """
+    Return a duckdb connection object
+    """
+    duckdb_connection = duckdb.connect(database=':memory:')
+    duckdb_connection.execute("""
+        INSTALL httpfs;
+        LOAD httpfs;
+        SET s3_region='us-east-1';
+        SET s3_access_key_id='{}';
+        SET s3_secret_access_key='{}';
+    """.format(os.environ['S3_USER'], os.environ['S3_ACCESS'])
+    )
+
+    return duckdb_connection
+
+
+def handler(event, context):
+    """
+    Run a SQL query in a memory db as a serverless function
+    """
+    is_warm = False
+    # run a timer for info
+    start = time.time()
+    global con
+    if not con:
+        # create a new connection
+        con = return_duckdb_connection()
+    else:
+        # return to the caller the status of the lambda
+        is_warm = True
+
+    # get the query to be executed from the payload
+    event_query = event.get('q', None)
+    limit = int(event.get('limit', DEFAULT_LIMIT))
+    results = []
+    if not event_query:
+        print("No query provided, will return empty results")
+    else:
+        # execute the query and return a pandas dataframe
+        # take the limit, to avoid crashing the lambdas by returning too many results
+        _df = con.execute(event_query).df().iloc[:limit]
+        results = convert_records_to_json(_df)
+    
+    # return response to the client with metadata
+    return wrap_response(start, event_query, results, is_warm)
+
+
+def convert_records_to_json(_df):
+    if len(_df) > 0:
+        # convert every timestamp to string to avoid serialization issues
+        cols = [col for col in _df.columns if _df[col].dtype == 'datetime64[ns]']
+        _df = _df.astype({_: str for _ in cols})
+
+    return _df.to_dict('records')
+
+
+def wrap_response(start, event_query, results, is_warm):
+    """
+    Wrap the response in a format that can be used by the client
+    """
+    return {
+        "metadata": {
+            "timeMs": int((time.time() - start) * 1000.0),
+            "epochMs": int(time.time() * 1000),
+            "eventId": str(uuid.uuid4()),
+            "query": event_query,
+            "warm": is_warm
+        },
+        "data": {
+            "records": results
+        }
+    }
